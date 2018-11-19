99b14b329bda15c2abe335a399e8934fba1f71b5
Carlos Perez <solracias@gmail.com>
2018-11-18 20:27:29 -0800

    test case 2 complete

---
+++ tests/artsyJewels/testCase2.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "@tags": ["testcase2"],
+  "search functionality is working": function(client) {
+    client
+      .url("https://www.artsyjewels.com")
+      .waitForElementVisible("body", 1000)
+      .waitForElementVisible('a[href*="/search"]', 1000)
+      .click('a[href*="/search"]')
+      .waitForElementVisible("[type=search]", 1000)
+      .setValue("[type=search]", ["silver bracelets", client.Keys.ENTER])
+      .end();
+  }
+};
