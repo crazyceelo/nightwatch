ea26a8034d1297b4dec318440dd7a428acf41fad
Carlos Perez <solracias@gmail.com>
2018-11-18 21:25:09 -0800

    test case 3 done and added a gitignore file and a credentials file for name and passwords

---
+++ tests/artsyJewels/testCase3.js
@@ -0,0 +1,16 @@
+const credentials = require("../../config/credentials");
+
+module.exports = {
+  "@tags": ["testcase3"],
+  "user can login with valid email and password": function(client) {
+    client
+      .url("https://www.artsyjewels.com/account/login")
+      .waitForElementVisible("body", 1000)
+      .waitForElementVisible("#CustomerEmail", 1000)
+      .setValue("#CustomerEmail", credentials.email)
+      .waitForElementVisible("#CustomerPassword", 1000)
+      .setValue("#CustomerPassword", credentials.password)
+      .waitForElementVisible('[value="Sign In"]', 1000)
+      .click('[value="Sign In"]');
+  }
+};
